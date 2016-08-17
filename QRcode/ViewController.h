efffe190ebeb68a3dbc57c9fdee976693379ebab
hzf <1810022686@qq.com>
2016-08-17 11:43:23 +0800

    Initial Commit

---
+++ QRcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRcode
+//
+//  Created by hzf on 16/8/17.
+//  Copyright © 2016年 hzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
